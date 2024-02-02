c7fe3cb047b3f8b81186e18e314fa446e3b3b790
afni y <145626482+triafniyuliana@users.noreply.github.com>
2024-02-02 16:47:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
       event.preventDefault();
   
-      var name = document.getElementById('nama').value;
+      var nama = document.getElementById('nama').value;
       var email = document.getElementById('email').value;
-      var message = document.getElementById('pesan').value;
+      var pesan = document.getElementById('pesan').value;
   
       if (!nama || !email || !pesan) {
           alert('Please fill out all the required fields.');
